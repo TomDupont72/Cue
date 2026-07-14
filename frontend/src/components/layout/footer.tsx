c18e90d00f8fdfa56c1a31097d123dfd7e0ea363
Tom Dupont <tomdupont72@gmail.com>
2026-07-14 23:55:54 +0200

    feat(FRONT): make base ui

---
+++ frontend/src/components/layout/footer.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./container";
+
+export default function Footer() {
+  return (
+    <footer className="border-t">
+      <Container className="flex min-h-16 items-center text-sm text-muted-foreground">
+        Cue
+      </Container>
+    </footer>
+  );
+}
---
+++ frontend/src/components/layout/header.tsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+import { Container } from "./container";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle
+} from "../ui/navigation-menu";
+import cueLogo from "@/assets/cue-logo.png"
+
+export default function Header() {
+  return (
+    <header className="border-b border-border bg-background">
+      <Container className="flex h-16 items-center justify-between">
+        <div className="flex  items-center justify-between">
+          <Link to="/" className="text-xl font-semibold">
+            <img src={cueLogo} className="h-8"/>
+          </Link>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle()}
+                  render={<Link to="/dashboard">Dashboard</Link>}
+                />
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle()}
+                  render={<Link to="/series">Séries</Link>}
+                />
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle()}
+                  render={<Link to="/calendar">Calendrier</Link>}
+                />
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle()}
+                  render={<Link to="/search">Recherche</Link>}
+                />
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            render={
+              <Button variant="ghost" size="icon" className="rounded-full">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="shadcn" />
+                  <AvatarFallback>LR</AvatarFallback>
+                </Avatar>
+              </Button>
+            }
+          />
+          <DropdownMenuContent align="end">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>Profil</DropdownMenuItem>
+              <DropdownMenuItem>Se déconnecter</DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </Container>
+    </header>
+  );
+}
