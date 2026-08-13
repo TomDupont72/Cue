5bb287bed51e487da58e6695ec0d3ddc13e7abbc
Tom Dupont <tomdupont72@gmail.com>
2026-08-14 00:33:36 +0200

    fix(FRONT): preserve order in feed

---
+++ frontend/src/features/user/utils/episodeFeedOrder.ts
@@ -0,0 +1,36 @@
+import type { UserEpisodesFeedGetItem, UserEpisodesFeedGetResponse } from "../types/user.types";
+
+function preserveSectionOrder(
+  previous: UserEpisodesFeedGetItem[],
+  next: UserEpisodesFeedGetItem[]
+) {
+  const nextBySeriesId = new Map(next.map((item) => [item.seriesId, item]));
+  const previousIds = new Set(previous.map((item) => item.seriesId));
+
+  const newItems = next.filter((item) => !previousIds.has(item.seriesId));
+
+  const existingItems = previous.flatMap(({ seriesId }) => {
+    const freshItem = nextBySeriesId.get(seriesId);
+    return freshItem ? [freshItem] : [];
+  });
+
+  return [...newItems, ...existingItems];
+}
+
+export function preserveFeedOrder(
+  previousData: unknown | undefined,
+  newData: unknown
+): UserEpisodesFeedGetResponse {
+  const next = newData as UserEpisodesFeedGetResponse;
+  const previous = previousData as UserEpisodesFeedGetResponse | undefined;
+
+  if (!previous) {
+    return next;
+  }
+
+  return {
+    watching: preserveSectionOrder(previous.watching, next.watching),
+    paused: preserveSectionOrder(previous.paused, next.paused),
+    dropped: preserveSectionOrder(previous.dropped, next.dropped)
+  };
+}
